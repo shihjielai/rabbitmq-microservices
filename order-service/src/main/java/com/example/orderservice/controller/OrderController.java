a0bfa7a6ec4f12a7232e5bb38f43f7025d08decb
Jay <shihjielai@gmail.com>
2022-09-12 15:25:17 +0800

    order service - create REST api to send message

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,39 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.Order;
+import com.example.orderservice.dto.OrderEvent;
+import com.example.orderservice.producer.OrderProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private final OrderProducer orderProducer;
+
+    @Autowired
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order) {
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("Order is in pending status");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order send to the RabbitMQ...";
+    }
+}
